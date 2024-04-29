a7825be0373bd9338a16e678ce4a8698cd2367fd
salman <msal782692@gmail.com>
2024-04-29 08:02:34 -0700

    Created project 2 initial files and
    Added project 2 link in main index , Created index.html , style.css , script.js complected HTmL project 2

--- project 1/script.js
+++ project 1/script.js
@@ -19,9 +19,14 @@ function showError(input , message) {
 }
 
 //Function to check if email is valid
-function isValidEmail(email){
+function checkEmail(input) {
     (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
+    if ( re.test(input.value.trim () ) ) {
+        showSuccess(input);
+    } else {
+        showError(input,` please provide a valid email`)
+    }
 }
 
 // function to update class for success
@@ -34,8 +39,35 @@ function showsuccess(input) {
 //function to check if required field have data
 function checkRequired(inputarray) {
     inputarray.foreach(function(input)  {
-        
+        if (input.value === '') {
+            console.log(input.id);
+            showError(input,`${getField(input)} is required`);
+        } else {
+            showsuccess(input);
+        }
     });
+}
+
+//function to check length of input field
+function checklength(input,min,max) {
+    if (input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length > max)
+        showError(input,`${getfieldId(input)} needs to be less ${min} characters`);
+}     else {
+    showsuccess(input);
+}
+
+//function to check if password and confirm password
+function checkpasswordmatch(input1,input2) {
+    if (input1.value !== input2.value) {
+        showError(input2,"password don't match")
+    }
+}
+
+//function to get id of the input field with proper case
+function getFieldID(input) {
+    return input.id.carAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // create event listener for submit button 
@@ -44,6 +76,9 @@ form.addEventListener('submit', function(e) {
     e.prventdefault();
 
     checkRequired([username,email,password,password2]);
-
+    checklength(username,3,10);
+    checklength(password,6,10);
+    checkEmail(email);
+    checkpasswordmatch(password,password2);
 
 })
